936d56e7b797f72b9bb89ffc3669198e0d9a7b9c
kenethnic <79937993+kennethnico@users.noreply.github.com>
2022-06-06 14:17:32 -0500

    Padding top-bottom

--- action.php
+++ action.php
@@ -1,5 +1,19 @@
 <?php
  function db_connect(){
-
+     $server = "172.19.202.77";
+     $user = "registros";
+     $password = "R3g1xtr0s!";
+     //$password = "Pjcdmx@22!";
+     $dbName = "registros";
+     try{
+         $conexion = new PDO("mysql:host=$server;dbname=$dbName", $user,$password);
+         $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+         echo "Conexión realizada satisfactoriamente";
+     }
+     catch(PDOException $e){
+         echo "La conexión ha fallado: ". $e->getMessage();
+     }
  }
+
+ db_connect();
 ?>
